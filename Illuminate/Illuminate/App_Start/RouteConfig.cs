20bf6410484579e68d09a39d4b0f73c570586454
emilydesmond <emilyjadesmond@gmail.com>
2019-05-16 09:53:40 -0700

    Updated RouteConfig file so default landing page is Welcome page

--- Illuminate/Illuminate/App_Start/RouteConfig.cs
+++ Illuminate/Illuminate/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Illuminate
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Dashboard", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Welcome", id = UrlParameter.Optional }
             );
         }
     }
